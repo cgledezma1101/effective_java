363ce245c46defd09a1f4b50790be81aa17649de
Carlos Ledezma <cgledezma1101@gmail.com>
2018-04-14 18:13:10 +1000

    Added typesafe alternatives to varargs examples

--- chapter_5/GenericVarargs.java
+++ chapter_5/GenericVarargs.java
@@ -18,6 +18,17 @@ public class GenericVarargs {
 		throw new AssertionError(); // Can't get here
 	}
 
+	// This is an alternative, safe version of pickTwo
+	static <T> List<T> pickTwo(T a, T b, T c) {
+		switch (ThreadLocalRandom.current().nextInt(3)) {
+			case 0: return List.of(a, b);
+			case 1: return List.of(a, c);
+			case 2: return List.of(b, c);
+		}
+
+		throw new AssertionError(); // Can't get here
+	}
+
 	// This is safe because (a) it doesn't store anything in the varargs array and 
 	// (b) it doesn't expose the varargs array
 	// Annotating with SafeVarargs prevents the warning
@@ -31,8 +42,18 @@ public class GenericVarargs {
 		return result;
 	}
 
+	// Alternatively, the method can be implemented using generic lists
+	// This can be used along witht List.of(T...)
+	static <T> List<T> flatten(List<List<? extends T>> lists) {
+		List<T> result = new ArrayList<>();
+		for (List<? extends T> list : lists) {
+			result.addAll(list);
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) {
-		// This throws a ClassCastException because the return type of toArray is determined to be Object[]
-		String[] attributes = pickTwo("Good", "Fast", "Cheap");
+		String[] attributes = pickTwo("Good", "Fast", "Cheap"); // This throws a ClassCastException because the return type of toArray is determined to be Object[]
 	}
 }
