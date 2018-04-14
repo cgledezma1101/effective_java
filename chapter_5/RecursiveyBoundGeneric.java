86e893cd2852859072ae09a2ac917604ba9c0ca2
Carlos Ledezma <cgledezma1101@gmail.com>
2018-04-14 16:22:09 +1000

    Example for recursively bound generics

---
+++ chapter_5/RecursiveyBoundGeneric.java
@@ -0,0 +1,19 @@
+import java.util.Collection;
+import java.util.Objects;
+
+public class RecursivelyBoundGeneric {
+	public static <E extends Comparable<E>> E max(Collection<E> c) {
+		if (c.isEmpty()) {
+			throw new IllegalArgumentException("Empty collection");
+		}
+
+		E result = null;
+		for (E e : c) {
+			if (result == null || e.compareTo(result) > 0) {
+				result = Objects.requireNonNull(e);
+			}
+		}
+
+		return result;
+	}
+}
