d663bfde688b272add855f4f9b42260f36c71c24
Carlos Ledezma <cgledezma1101@gmail.com>
2018-04-14 16:13:10 +1000

    Example on how to create a generic factory

---
+++ chapter_5/IdentityFunction.java
@@ -0,0 +1,10 @@
+import java.util.function.UnaryOperator;
+
+public class IdentityFunction {
+	private static UnaryOperator<Object> IDENTITY_FN = (t) -> t;
+
+	@SuppressWarnings("unchecked")
+	public static <T> UnaryOperator<T> identityFunction() {
+		return (UnaryOperator<T>) IDENTITY_FN;
+	}
+}
