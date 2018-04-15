989283ffabc897cc087e22532fbe8bc2124f3e61
Carlos Ledezma <cgledezma1101@gmail.com>
2018-04-15 17:48:11 +1000

    Example for constant specific methods

---
+++ chapter_6/ConstantSpecificMethods.java
@@ -0,0 +1,24 @@
+public class ConstantSpecificMethods {
+	public static enum Operation {
+		PLUS("+") { public double apply(double x, double y){ return x + y; } },
+		MINUS("-") { public double apply(double x, double y){ return x - y; } },
+		TIMES("*") { public double apply(double x, double y){ return x * y; } },
+		DIVIDE("/") { public double apply(double x, double y){ return x / y; } };
+
+		private final String symbol;
+
+		Operation(String symbol) { this.symbol = symbol; }
+
+		@Override public String toString(){ return symbol; }
+
+		public abstract double apply(double x, double y);
+	}
+
+	public static void main(String[] args) {
+		double x = Double.parseDouble(args[0]);
+		double y = Double.parseDouble(args[1]);
+		for (Operation op : Operation.values()) {
+			System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+		}
+	}
+}
